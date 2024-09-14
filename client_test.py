a37c97245cc0f800d54bb787cc9d166563debbb7
Unknownuser10132 <63773788+Unknownuser10132@users.noreply.github.com>
2024-09-14 21:38:38 +0800

    Optional task 1

--- client_test.py
+++ client_test.py
@@ -8,17 +8,45 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      expected_values = (
+          quote['stock'],
+          float(quote['top_bid']['price']),
+          float(quote['top_ask']['price']),
+          (float(quote['top_bid']['price']) + float(quote['top_ask']['price'])) / 2
+        )
+      self.assertEqual(getDataPoint(quote), expected_values)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+      {'top_ask': {'price': 117.87, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 121.68, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      expected_values = (
+          quote['stock'],
+          float(quote['top_bid']['price']),
+          float(quote['top_ask']['price']),
+          (float(quote['top_bid']['price']) + float(quote['top_ask']['price'])) / 2
+        )
+      self.assertEqual(getDataPoint(quote), expected_values)
 
   """ ------------ Add more unit tests ------------ """
-
+  def test_getDataPoint_calculatePriceBidEqualToAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 119.2, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 117.87, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      expected_values = (
+          quote['stock'],
+          float(quote['top_bid']['price']),
+          float(quote['top_ask']['price']),
+          (float(quote['top_bid']['price']) + float(quote['top_ask']['price'])) / 2
+        )
+      self.assertEqual(getDataPoint(quote), expected_values)
 
 
 if __name__ == '__main__':
